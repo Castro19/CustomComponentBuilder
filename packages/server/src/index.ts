7495dc6dab1e4747be7b0464d1d2bf7b5d8e89f4
Castro19 <ccastroo@calpoly.edu>
2024-11-24 12:01:08 -0800

    RenderPage & Header

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -3,12 +3,15 @@ import express, { Request, Response } from "express";
 import componentRouter from "./routes/component";
 import buttonRouter from "./routes/button";
 import { connect } from "./services/mongo";
+import path from "path";
 
 connect("publish-ui");
 
 const app = express();
 const port = process.env.PORT || 3000;
 const staticDir = process.env.STATIC || "public";
+
+console.log("STATIC DIR", staticDir);
 
 app.use(express.static(staticDir));
 app.use(express.json());
@@ -23,3 +26,7 @@ app.listen(port, () => {
 
 app.use("/component", componentRouter);
 app.use("/button", buttonRouter);
+
+app.use("/", (req: Request, res: Response) => {
+  res.sendFile(path.join(staticDir, "index.html"));
+});
