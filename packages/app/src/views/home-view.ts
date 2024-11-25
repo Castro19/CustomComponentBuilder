c1d710c54f65de21870c39aa28badfa26feb2416
Castro19 <ccastroo@calpoly.edu>
2024-11-25 10:41:06 -0800

    home-view

--- packages/app/src/views/home-view.ts
+++ packages/app/src/views/home-view.ts
@@ -1,105 +1,36 @@
 import { Auth, Observer } from "@calpoly/mustang";
-import { css, html, LitElement } from "lit";
+import { html, LitElement } from "lit";
 import { state } from "lit/decorators.js";
-
-// import reset from "../styles/reset.css.js";
+import reset from "../styles/reset.css.js";
+import frontPage from "../styles/frontPage.css.js";
+import gridUtility from "../styles/gridUtility.css.js";
 
 export class HomeViewElement extends LitElement {
-  src = "/api/tours";
-
   @state()
-  //   tourIndex = new Array<Tour>();
   _authObserver = new Observer<Auth.Model>(this, "blazing:auth");
 
   _user = new Auth.User();
 
-  connectedCallback() {
-    super.connectedCallback();
-    this._authObserver.observe(({ user }) => {
-      if (user) {
-        this._user = user;
-      }
-      //   this.hydrate(this.src);
-    });
-  }
-
-  //   hydrate(url: string) {
-  //     fetch(url, {
-  //       headers: Auth.headers(this._user),
-  //     })
-  //       .then((res: Response) => {
-  //         if (res.status === 200) return res.json();
-  //         throw `Server responded with status ${res.status}`;
-  //       })
-  //       .catch((err) => console.log("Failed to load tour data:", err))
-  //       .then((json: unknown) => {
-  //         if (json) {
-  //           console.log("Tours:", json);
-  //           const { data } = json as { data: Array<Tour> };
-  //           this.tourIndex = tours;
-  //         }
-  //       })
-  //       .catch((err) => console.log("Failed to convert tour data:", err));
-  //   }
-
   render() {
-    // const tourList = this.tourIndex.map(this.renderItem);
-
     return html`
-      <main class="page">
-        <header>
-          <h2>Your Trips</h2>
-        </header>
-        <dl></dl>
+      <main>
+        <div class="magazine-page magazine-page-right magazine-cover">
+          <h1 class="title">PublishUI</h1>
+          <h2 class="subtitle-cover">Another UI Component Library</h2>
+          <div class="footer-container-cover">
+            <p class="author-cover">By Cristian Castro Oliva</p>
+            <button class="arrow-button">
+              <a href="pages/tableContents/tableContents.html">
+                <svg class="icon arrow" width="50" height="50">
+                  <use href="/componentOptions.svg#icon-arrow" />
+                </svg>
+              </a>
+            </button>
+          </div>
+        </div>
       </main>
     `;
   }
 
-  renderItem(_t: any) {
-    return html`<div>Hello</div>`;
-  }
-
-  static styles = [
-    // reset.styles,
-    css`
-      :host {
-        display: contents;
-      }
-      main.page {
-        display: grid;
-        grid-column: 1/-1;
-        padding: var(--size-spacing-small) var(--size-spacing-medium);
-        grid-template-columns: subgrid;
-        grid-template-rows: auto auto auto 1fr;
-        grid-template-areas:
-          "hd hd yr dt1 dt2 dd dd dd"
-          "xx xx yr dt1 dt2 dd dd dd";
-        gap: var(--size-spacing-medium) var(--size-spacing-large);
-        align-items: end;
-      }
-      header {
-        grid-area: hd;
-      }
-      dl {
-        display: contents;
-      }
-      dt {
-        grid-area: yr;
-        text-align: right;
-      }
-      dt + dt {
-        grid-area: dt1;
-        text-align: center;
-      }
-      dt + dt + dt {
-        grid-area: dt2;
-      }
-      dd {
-        grid-area: dd;
-      }
-      time {
-        white-space: nowrap;
-      }
-    `,
-  ];
+  static styles = [reset.styles, frontPage.styles, gridUtility.styles];
 }
